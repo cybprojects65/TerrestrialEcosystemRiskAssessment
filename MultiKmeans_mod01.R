ae1a21ea50b70058fcb4ffaa7b86d9fe6be20f7f
gianpaolocoro <gianpaolo.coro@isti.cnr.it>
2025-07-14 23:40:26 +0200

    multi k-means on the entire dataset

--- MultiKmeans_mod01.R
+++ MultiKmeans_mod01.R
@@ -6,9 +6,12 @@ options(warn = -1)
 # File to select
 variables_risk1_standardized <- read.csv(file="./input/variables_risk1_standardized.csv", header=TRUE, sep=",")
 
-# Remove rows with NA values
 variables_risk1_standardized <- variables_risk1_standardized %>%
   drop_na()
+
+# Remove rows with NA values
+# variables_risk1_standardized <- variables_risk1_standardized %>%
+#   drop_na()
 
 # Selection of my study dataframe
 # Bounding box coordinates
@@ -18,13 +21,13 @@ variables_risk1_standardized <- variables_risk1_standardized %>%
 # bbymax=90
 
 # Year of interest
-year = "risk1_2024_pr"
+year = "risk1_on_VAE_2024_pr"
 
 # Selection of my study area and features
 selection <- variables_risk1_standardized #[which((variables_risk1_standardized$longitude>=bbxmin)&(variables_risk1_standardized$longitude<=bbxmax)&(variables_risk1_standardized$latitude>=bbymin)&(variables_risk1_standardized$latitude<=bbymax)), ]
 
 #select the first 300 lines to test the code=============Remember to comment
-selection <- selection %>% slice(1:100)
+#selection <- selection %>% slice(1:100)
 
 selected_features<-c("x",
                      "y",
@@ -40,13 +43,14 @@ selected_features<-c("x",
                      "out059_precipitation_cum_relative_change_2024pr_rcp85",
                      "out065_number_species_cum_all_2024pr",
                      "out147_land_use_and_cover_change_2024pr"
-                     )
+)
 
 cat(paste0("***Initialization***", "\n"))
 
 # n_centroidi <- 7
-multi_centroidi<-  seq(from=21, to=21, by=1) #c(4)
-N <- 2
+#multi_centroidi<-  seq(from=20, to=30, by=2) #c(4)
+multi_centroidi<-  seq(from=5, to=29, by=1) #c(4)
+N <- 1000
 
 bics<-c()
 for (n_centroidi in multi_centroidi){
@@ -68,75 +72,14 @@ for (n_centroidi in multi_centroidi){
     
   }
   
+  km <- kmeans(as.matrix(v), centers = as.matrix(centroidi), iter.max = N)
+  # Assign the column with the centroid value to the original dataset.
+  selected_features_coords$distance_class <- km$cluster
+  v$distance_class <- km$cluster
   
+  centroidi<-as.matrix(km$centers)
   
-  # Empty vector to be filled with the assignment of the centroid for each row of v
-  d <- numeric(length = nrow(v))
-  
-  v$distance_class <- NA
-  prev_centr_distr<-rep(0,n_centroidi)
-  for (k in 1:N) {
-    cat(paste0("I am executing loop number ", k, "\n"))
-    
-    cat(paste0("***Assignment***", "\n"))
-    
-    for (punto in 1:nrow(v)) {
-      
-      distanze<-sapply(1:n_centroidi, function(centroide){
-        
-        d_vi_centroide = sqrt( sum ((v[punto,1:(ncol(v)-1)]-centroidi[centroide,])*(v[punto,1:(ncol(v)-1)]-centroidi[centroide,])) )
-        return(d_vi_centroide)
-        
-      },simplify = T)
-      
-      d[punto] <- which(distanze == min(distanze))[1]  # Index of the smallest value to assign to each point
-    }
-    
-    cat(paste0("***FILE***", "\n"))
-    
-    # Assign the column with the centroid value to the original dataset.
-    selected_features_coords$distance_class <- d
-    
-    v$distance_class <- d
-    
-    # Empty matrix to store the averages
-    v_medie <- matrix(0,nrow = nrow(centroidi), ncol = ncol(centroidi))
-    centroid_distribution<-c()
-    for (centroide in 1:nrow(centroidi)) {
-      cat(paste0("---- I am examining the centroid ", centroide, "\n"))
-      # Take the rows with the centroid of interest: rows = the row of the centroid, columns = v minus the column with the centroid number
-      v_centroide <- v[v$distance_class==centroide, -which(colnames(v)=="distance_class")]
-      v_centroide<-as.matrix(v_centroide)
-      if (nrow(v_centroide)==0){
-        cat(paste0("The points assigned to the centroid ", centroide, " are ", nrow(v_centroide), "\n"))
-        v_medie[centroide,]<-centroidi[centroide,]
-        centroid_distribution<-c(centroid_distribution,0)
-      }else{
-        # Calculate the mean on this new set of data selected for the centroid.
-        cat(paste0("The points assigned to the centroid ", centroide, " are ", nrow(v_centroide), "\n"))
-        medie_centroide<-apply(v_centroide,2,mean)
-        v_medie[centroide,] <- medie_centroide
-        centroid_distribution<-c(centroid_distribution,nrow(v_centroide))
-      }
-    }
-    cat(paste0("***Update***", "\n"))
-    centroidi <- v_medie
-    
-    if (length(which(centroid_distribution!=prev_centr_distr))==0){
-      cat(paste0("***Convergence***", "\n"))
-      break
-    }else{
-      prev_centr_distr<-centroid_distribution
-    }
-  }
-  
-  centroidi_df<-as.data.frame(centroidi)
-  names(centroidi_df) <- names(v)[1:(length(v)-1)]
-  
-  ks.test(centroid_distribution, "punif")
-  
-  #############
-  
+  #ks.test(centroid_distribution, "punif")
   
   # Quantiles
   v_quantili <- apply(v, 2, quantile)
@@ -213,7 +156,7 @@ for (n_centroidi in multi_centroidi){
   centroidi_annotated$risk_level <- centroide_interpretazione
   
   write.csv(centroidi_annotated, 
-            file = paste0("./output/MultiKmeans/centroidi_annotated_", n_centroidi, "_", year, ".csv"), 
+            file = paste0("./output/all_centroidi_annotated_", n_centroidi, "_", year, ".csv"), 
             row.names = FALSE)
   
   # Assign the distance class interpretation to the original dataset
@@ -227,9 +170,10 @@ for (n_centroidi in multi_centroidi){
   cat("Saving the dataset\n")
   nuovo_v <- cbind(selected_features_coords[,1:2],v)
   names(nuovo_v)<-c(names(selected_features_coords),"distance_class_interpretation")
-  output_file<-paste0("./output/MultiKmeans/centroid_classification_assignment_",n_centroidi,"_", year, ".csv")
+  output_file<-paste0("./output/all_centroid_classification_assignment_",n_centroidi,"_", year, ".csv")
   write.csv(nuovo_v, output_file, row.names = F)
   
+  centroid_distribution<-km$size
   #### CALCULATING ChiSqr
   if (length(which(centroid_distribution<=2))>0 || 
       ( (min(centroid_distribution)/max(centroid_distribution) ) <0.007) 
@@ -240,10 +184,28 @@ for (n_centroidi in multi_centroidi){
     centroid_distribution.norm<-centroid_distribution/sum(centroid_distribution)
     reference<-rep(mean(centroid_distribution),length(centroid_distribution) )
     reference.norm<-reference/sum(reference)
-    chi<-chisq.test(centroid_distribution.norm, p = reference.norm)
-    bic<-chi$p.value
+    chisq<-sum((centroid_distribution.norm*1000-reference.norm*1000)^2/(reference.norm*1000))/length(centroid_distribution.norm)
+    #high chisqr-> worse agreement with uniform distr
+    #since we are selecting the maximum, let's invert the unif
+    bic<-1/chisq
+    
+    #EXPLANATION OF THE CHI SQR CRITERION:
+    #chi sqr probability calculation: for study purposes
+    #the probability that the chisqr is lower than the calculcation has the inverse trend of the chisqr value
+    #a small chisqr calculated means a higher probability of matching
+    #a high chisqr calculated means a lower probability of matching
+    #let's calculate the P(chisqr>chisqr_calculated), because the theoretical expected value of chisqr is 1
+    #if this prob is high, chisqr_calculated is consistent with the expected value-> uniform distribution
+    #if it is low then the chisqr_calculated is too far from the expected value-> non uniform distribution
+    #uncomment for verification and testing
+    #p_value <- pchisq(chisq, df = length(centroid_distribution.norm)-1, lower.tail = FALSE)
+    #invert the criterion: high bic should be preferred because it corresponds to low p_value
+    #bic<-p_value
+    #cat("pvalue:",p_value,"\n")
+    
+    cat("Centroid distribution:",centroid_distribution.norm,"\n")
   }
-  cat("ChiSqr:",bic,"\n")
+  cat("Unif:",bic,"\n")
   bics<-c(bics,bic)
   cat("Done\n")
 }
@@ -257,4 +219,4 @@ cat("ChiSQRs: ",bics,"\n")
 cat("Best clustering: K=",best_clusterisation,"\n")
 best_clusterisation_file = paste0("./centroid_classification_assignment_",best_clusterisation,".csv")
 cat("Best clustering file to take as result:",best_clusterisation_file,"\n")
-
+plot(multi_centroidi,bics,type='l')
