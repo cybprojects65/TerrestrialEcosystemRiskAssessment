39bbbbf58eae695f9aa2cea55af2d24f68345ffa
gianpaolocoro <gianpaolo.coro@isti.cnr.it>
2025-07-20 00:29:38 +0200

    log added

--- MultiKmeans_mod01.R
+++ MultiKmeans_mod01.R
@@ -201,7 +201,7 @@ for (n_centroidi in multi_centroidi){
     p_value <- pchisq(chisq, df = length(centroid_distribution.norm)-1, lower.tail = FALSE)
     #invert the criterion: high bic should be preferred because it corresponds to low p_value
     bic<-p_value
-    #cat("pvalue:",p_value,"\n")
+    cat("pvalue:",p_value,"\n")
     
     cat("Centroid distribution:",centroid_distribution.norm,"\n")
   }
